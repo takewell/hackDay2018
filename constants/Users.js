3efdb5ae03a9ffe41ff5bab5d5c73b37914087d7
takewell <takewell.dev@gmail.com>
2018-12-16 06:38:17 +0900

    Add User json

---
+++ constants/Users.js
@@ -0,0 +1,12 @@
+export default {
+  users: [
+    {
+      mail: 'yasu@yasu.com', password: 'pn0EpKroRNlojqup4aYgumsho82ydAa0FW3X0D-P3x8='
+    }, {
+      mail: 'yasuno@gmail.com', password: 'V9IrpV-z1O0SFxkhM83EQlstB36KLflSfQO3YFcqeW8='
+    },
+    {
+      mail: 'yasuno0327@gmail.com', password: 'ppsVZSGoJwS6tYMT3WqvuhlvNqOdckmMoNgeeuTaN-Y='
+    }
+  ]
+};
